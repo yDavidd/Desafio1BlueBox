56fedbb2168becc5421d5e8daa02daedbe906f55
yDavidd <132359951+yDavidd@users.noreply.github.com>
2024-05-23 14:29:49 -0300

    BlueBox DAVID

---
+++ src/main/java/com/example/bb/BlueBoxAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBoxAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBoxAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bb/BlueBoxAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBoxAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
